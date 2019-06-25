849a71613f5d09a9c61e48ecfa50d3bdb5b7eed3
Alan Xu <alanxu1220@gmail.com>
2019-06-24 19:55:18 -0700

    6-24 NL1
    Can now output visit notes and rank their according to their frequencies (ranking now implemented in exp/panda_exp).

--- ReportFile.py
+++ ReportFile.py
@@ -1,13 +1,20 @@
 """ Module for the ReportFile class """
-
 import pandas
 
+class ReportFileError(Exception):
+    pass
+
+class NotWCReport(ReportFileError):
+    pass
+
+
 class ReportFile:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, is_wcreport: bool=True):
+        self.is_wcreport = is_wcreport
+
         inter_file = pandas.ExcelFile(file_path)
 
         try:
-
             self.sheets = dict()
             for name in inter_file.sheet_names:
                 self.sheets[name] = inter_file.parse(name)
@@ -15,6 +22,20 @@ class ReportFile:
         finally:
             inter_file.close()
 
+
     def __getitem__(self, item):
         return self.sheets[item]
+
+
+    def get_visitNotes(self, quarter: str):
+        """Only applicable to UC Irvine Writing Center report files."""
+
+        """Returns a tuple of visit notes, stripped of any other info."""
+        if not self.is_wcreport:
+            raise NotWCReport
+
+        return tuple(i for i in self.sheets[quarter]["VisitNotes"])
+
+
+
 
--- exp/panda_exp.py
+++ exp/panda_exp.py
@@ -1,6 +1,7 @@
-import pandas
+
 
 """
+import pandas
 ser = pandas.Series(["soo", 200], ["Nancy", "Boo"])
 ser = ser * 2
 print(ser.__str__().split())
@@ -13,9 +14,32 @@ df = pandas.DataFrame(d)
 print(df)
 
 """
-
+"""
+import pandas
 intermediate = pandas.ExcelFile("../reports/report.xlsx")
 file = intermediate.parse(intermediate.sheet_names[0])
 
-row_0 = file.iloc[0]
-print(str(row_0))
+columns = file
+"""
+
+import ReportFile
+from collections import defaultdict
+
+report_file = ReportFile.ReportFile("../reports/report.xlsx")
+entries = report_file.get_visitNotes("FQ18")
+
+phrase_freq_dict = defaultdict(int)
+word_freq_dict = defaultdict(int)
+for entry in entries:
+    words = str(entry).split()
+    for word in words:
+        word_freq_dict[word] += 1
+    for index in range(len(words) - 1):
+        phrase = words[index] + ' ' + words[index + 1]
+        phrase_freq_dict[phrase] += 1
+
+ranked_phrase_freq = sorted(phrase_freq_dict.items(), key=lambda x: -x[1])
+ranked_word_freq = sorted(word_freq_dict.items(), key=lambda x: -x[1])
+
+print(ranked_phrase_freq)
+print(ranked_word_freq)
