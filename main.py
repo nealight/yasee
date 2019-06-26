07e6ed7fac9ba4128d40452ee418990aa7f3c18a
Alan Xu <alanxu1220@gmail.com>
2019-06-26 11:03:16 -0700

    Beta 1.0
    - The project now includes a dependencies.txt that will guide the user to configure a local Windows python environment, on which they will be able to run our visualization program.
    
    Future direction:
    1. Visualize word frequency using a chart.
    2. Visualize word frequency in relation to a specific column in the Excel form.
    3. Work on documentation.

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ class Console:
     def __call__(self, *args, **kwargs):
         while True:
             user_prompt = input().strip().lower()
-            if user_prompt in frozenset(("seeya", "see you")):
+            if (user_prompt in frozenset(("seeya", "see you"))):
                 break
 
-            if user_prompt == "generate word cloud":
+            if (user_prompt == "generate word cloud"):
                 print("Where is your file?")
                 target_file_path = input().strip()
                 print("Which excel sheet to analyze?")
@@ -21,6 +21,8 @@ class Console:
                 print("What would be the name of your cloud?")
                 output_path = input().strip()
                 word_cloud.store_wordcloud(output_path)
+
+
 
 
 if __name__ == "__main__":
