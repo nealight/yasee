7c00b76dd92dcba4d2379d7fb2edfdde5465081f
Alan Xu <alanxu1220@gmail.com>
2019-06-29 12:57:52 -0700

    6-29 NL3
    - Added an additional unittest for YaseeFreqCharts.
    
    Future direction:
    1. Support visualization of phrase frequency.
    2. Support visualization of word root frequency
    3. Visualize word frequency in relation to a specific category (Excel form column).

--- test_YaseeFreqCharts.py
+++ test_YaseeFreqCharts.py
@@ -11,6 +11,11 @@ class YaseeFreqChartsTest(unittest.TestCase):
         self.yfc.storeWordFreq("test/wordfreqchart.png")
         self.assertTrue(os.path.exists("test/wordfreqchart.png"))
 
+    def test_storeWordFreqTop20(self):
+        self.yfc = YaseeFreqCharts("test/report.xlsx", "FQ18", "VisitNotes")
+        self.yfc.storeWordFreq("test/wordfreqchart top20.png", 20)
+        self.assertTrue(os.path.exists("test/wordfreqchart top20.png"))
+
     def test_addStopWords(self):
         self.yfc.addStopWords("word")
         self.yfc.storeWordFreq("test/wordfreqcharexcluding'word'.png")
